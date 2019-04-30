b9c85d4e719c05fb37f5762d742fc32fe215870f
Nobuiko Sawai <nobuhiko.sawai@gmail.com>
2019-04-30 18:52:54 +0900

    Create simple chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import * as d3 from 'd3';
 import './App.css';
+
+type BarChartProps = {
+  data: Array<number>
+  width: number
+  height: number
+}
+
+const BarChart: React.FC<BarChartProps> = ({data, height, width}) => {
+  useEffect(() => {
+    drawChart();
+  }, []);
+
+  const drawChart = () => {
+    const svg = d3.select('body')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+      // .style('margin-left', 100);
+
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('x', (d, i) => i * 70)
+      .attr('y', (d, i) => height - 10 * d)
+      .attr('width', 65)
+      .attr('height', (d, i) => d * 10)
+      .attr('fill', 'green');
+
+    svg.selectAll('text')
+      .data(data)
+      .enter()
+      .append('text')
+      .text(d => d)
+      .attr('x', (d, i) => i * 70)
+      .attr('y', (d, i) => height - (10 * d) - 3);
+  };
+
+  return (
+    <div>
+    </div>
+  );
+}
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BarChart data={[12, 5, 6, 6, 9, 10]} width={700} height={500} />
     </div>
-  );
+  )
 }
 
 export default App;
